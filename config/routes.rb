c025e86cb9147420ffd4931186e46991867d5ca6
jobweeda <jobweeda@hotmail.com>
2017-11-19 12:48:21 +0100

    adds batches api index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 root to: "batches#index"
 
   devise_for :users
- 
+
+namespace :api do
+  resources :batches
+end
 
  resources :batches do
    resources :students
