d9b88d7cf33f4fb2984526593fb6dc2393c98872
jobweeda <jobweeda@hotmail.com>
2017-11-22 19:03:46 +0100

    tweaks

--- app/assets/javascripts/batches.js
+++ app/assets/javascripts/batches.js
@@ -1,28 +1,27 @@
-//
-// function createBatch(batchName){
-//
-//   var newBatch = { name: batchName };
-//   $.ajax({
-//     type: 'POST',
-//     url: '/batches.json',
-//     data: JSON.stringify({
-//       batch: newBatch
-//     }),
-//     contentType: 'application/json',
-//     dataType: 'json'
-//   });
-// }
-//
-// function submitBatch(event){
-//   event.preventDefault();
-//   var batchName = $('#new-batch').val();
-//
-//   createBatch(batchName);
-//   $('#new-batch').val(null)
-// }
-//
-//
-//
-//
-//
-// $('form').bind('submit', submitBatch);
+
+function createBatch(batchName){
+
+  $.ajax({
+    type: 'POST',
+    url: '/batches.json',
+    data: JSON.stringify({
+      batch: batchName
+    }),
+    contentType: 'application/json',
+    dataType: 'json'
+  });
+}
+
+function submitBatch(event){
+  event.preventDefault();
+  var batchName = $('#batch-form').val();
+
+  createBatch(batchName);
+  $('#batch-form').val(null)
+}
+
+
+
+
+
+$('form').bind('submit', submitBatch);
