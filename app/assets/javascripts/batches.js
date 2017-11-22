b8a8705ac3e44a9308db50a5dab0eebfecf33295
jobweeda <jobweeda@hotmail.com>
2017-11-22 19:11:32 +0100

    fix for adding students

--- app/assets/javascripts/batches.js
+++ app/assets/javascripts/batches.js
@@ -1,28 +1,28 @@
-
-function createBatch(batchName){
-
-  $.ajax({
-    type: 'POST',
-    url: '/batches.json',
-    data: JSON.stringify({
-      batch: batchName
-    }),
-    contentType: 'application/json',
-    dataType: 'json'
-  });
-}
-
-function submitBatch(event){
-  event.preventDefault();
-  var batchName = $('#batch-form').val();
-
-  createBatch(batchName);
-  $('#batch-form').val(null)
-}
-
-
-
-
-$(document).ready(function(){
-$('form').bind('submit', submitBatch);
-});
+//
+// function createBatch(batchName){
+//
+//   $.ajax({
+//     type: 'POST',
+//     url: '/api/batches.json',
+//     data: JSON.stringify({
+//       batch: batchName
+//     }),
+//     contentType: 'application/json',
+//     dataType: 'json'
+//   });
+// }
+//
+// function submitBatch(event){
+//   event.preventDefault();
+//   var batchName = $('#batch-form').val();
+//
+//   createBatch(batchName);
+//   $('#batch-form').val(null)
+// }
+//
+//
+//
+//
+// $(document).ready(function(){
+// $('form').bind('submit', submitBatch);
+// });
